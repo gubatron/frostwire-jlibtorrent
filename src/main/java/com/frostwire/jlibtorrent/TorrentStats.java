9dc088f9baa2d13703dbb0debce46de27fcb8aeb
haperlot <gmaximiliam15@gmail.com>
2016-12-17 09:59:40 -0400

    both get method added, there still place for improvement

--- src/main/java/com/frostwire/jlibtorrent/TorrentStats.java
+++ src/main/java/com/frostwire/jlibtorrent/TorrentStats.java
@@ -4,6 +4,7 @@ import com.frostwire.jlibtorrent.alerts.Alert;
 import com.frostwire.jlibtorrent.alerts.AlertType;
 import com.frostwire.jlibtorrent.alerts.BlockFinishedAlert;
 import com.frostwire.jlibtorrent.alerts.TorrentAddedAlert;
+import sun.invoke.empty.Empty;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -17,36 +18,26 @@ public final class TorrentStats {
     private TorrentHandle torrentHandle;
     private long samplingIntervalInMs;
     private long maxHistoryInMs;
-
     public static final String UPLOAD = "UPLOAD";
     public static final String DOWNLOAD = "DOWNLOAD";
-
-
-
     private Queue<Integer> downloadRate = new LinkedList<Integer>();
     private Queue<Integer> uploadRate = new LinkedList<Integer>();
-
-    //for collecting sampling interval time
-    private long tStart;
+    private long tStart; //for collecting sampling interval time
 
     public TorrentStats(final SessionManager sessionManager, TorrentHandle torrentHandle, long samplingIntervalInMs, long maxHistoryInMs) {
         this.sessionManager = sessionManager;
         this.torrentHandle = torrentHandle;
         this.samplingIntervalInMs = samplingIntervalInMs;
         this.maxHistoryInMs = maxHistoryInMs;
-        tStart = System.currentTimeMillis();
+        this.tStart = System.currentTimeMillis();
 
-        //start internal alert listener
         startAlertListener();
-
-
     }
 
 
     private void startAlertListener() {
 
         sessionManager.addListener(new AlertListener() {
-
             @Override
             public int[] types() {
                 return null;
@@ -64,28 +55,22 @@ public final class TorrentStats {
                         break;
 
                     case BLOCK_FINISHED:
-                        //only true if samplingIntervalInMs, margin of error 5-90 ms
+                        //only true if samplingIntervalInMs,as soon as I get it from the alert margin of error 5-90 ms.
                         if ((System.currentTimeMillis() - tStart) >= samplingIntervalInMs) {
                             tStart = System.currentTimeMillis();
 
-                            BlockFinishedAlert a = (BlockFinishedAlert) alert;
-                            int p = (int) (a.handle().status().progress() * 100);
-
-                            //if !paused && !finished
+                            //if !paused && !finished, it could be paused during the download
                             if (!torrentHandle.status().isFinished() && !torrentHandle.status().isPaused()) {
 
                                 downloadRate.add(torrentHandle.status().downloadRate());
                                 uploadRate.add(torrentHandle.status().uploadRate());
 
-
-                                System.out.println("Sampled time: " + downloadRate.size() * samplingIntervalInMs + " / maxHistory: "+ maxHistoryInMs);
-                                //if the sampling # exceeded the limit
-                                if(downloadRate.size() * samplingIntervalInMs > maxHistoryInMs){
+                                //if the sampling # exceeded the limit, remove head
+                                if (downloadRate.size() * samplingIntervalInMs > maxHistoryInMs) {
                                     downloadRate.poll();
                                     uploadRate.poll();
                                 }
 
-                                System.out.println("#of elements = "+ downloadRate.size());
 
                             }
 
@@ -104,9 +89,72 @@ public final class TorrentStats {
 
     //Type = DOWNLOAD / UPLOAD
     //limit = number of download speed samples or less if available
-    public int[] get(String type, int limit) {
+    public int[] get(String type) {
+        Queue<Integer> resultQueue = null;
+        int i;
 
-        return new int[1];
+        if (type.compareToIgnoreCase(this.UPLOAD) == 0)
+            resultQueue = uploadRate;
+        else if (type.compareToIgnoreCase(this.DOWNLOAD) == 0)
+            resultQueue = uploadRate;
+
+        if (!resultQueue.isEmpty()) {
+            int[] rateHistory = new int[resultQueue.size()];
+
+            //returning available items
+            i = 0;
+            for (Integer element : resultQueue) {
+                //System.out.println("element: "+element.intValue());
+                rateHistory[i] = element.intValue();
+
+                i++;
+            }
+
+            return rateHistory;
+        }
+
+        //return empty queue is empty
+        return new int[10];
+
+    }
+
+    public int[] get(String type, int limit) {
+        Queue<Integer> resultQueue = null;
+        int[] rateHistory=null;
+        int i, j;
+
+        if (type.compareToIgnoreCase(this.UPLOAD) == 0)
+            resultQueue = uploadRate;
+        else if (type.compareToIgnoreCase(this.DOWNLOAD) == 0)
+            resultQueue = uploadRate;
+
+        if (!resultQueue.isEmpty()) {
+
+            //returning available items if limit > itemList
+            if (limit >= resultQueue.size()) {
+                rateHistory = this.get(type);
+
+            } else if (limit < resultQueue.size()) {
+                rateHistory = new int[limit];
+                i = j = 0;
+
+                for (Integer element : resultQueue) {
+
+                    if (j >= resultQueue.size() - limit) {
+                        rateHistory[i] = element.intValue();
+                        i++;
+                    }
+                    j++;
+                }
+
+
+            }
+            return rateHistory;
+
+        }
+        //return empty queue is empty
+        return new int[limit];
+
     }
 
 
